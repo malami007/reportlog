6784bf3550628248d9b3d6317512770771096d11
malami007 <91380669+malami007@users.noreply.github.com>
2021-09-25 08:22:26 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE');
 header('Access-Control-Allow-Headers: X-Requested-With,Origin,Content-Type,Cookie,Accept');
 
 use PHPMailer\PHPMailer\PHPMailer;
